9408d70d8e0f9594082acf1f4447df04a89dd5c2
KuzeyKurt <japarov.s.i20@gmail.com>
2024-11-15 04:29:26 +0300

    в хедер добавлен навбар

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,17 +1,17 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
-import { Container } from './container';
-import { SearchInput } from './search-input';
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
-import { CartButton } from './cart-button';
-import { AuthModal } from './modals/auth-modal';
-import { ProfileButton } from './profile-button';
-import { useRouter, useSearchParams } from 'next/navigation';
-import toast from 'react-hot-toast';
+import { Container } from "./container";
+import { SearchInput } from "./search-input";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { CartButton } from "./cart-button";
+import { AuthModal } from "./modals/auth-modal";
+import { ProfileButton } from "./profile-button";
+import { useRouter, useSearchParams } from "next/navigation";
+import toast from "react-hot-toast";
 
 interface Props {
   hasSearch?: boolean;
@@ -19,25 +19,29 @@ interface Props {
   className?: string;
 }
 
-export const Header: React.FC<Props> = ({ className, hasSearch = true, hasCart = true }) => {
+export const Header: React.FC<Props> = ({
+  className,
+  hasSearch = true,
+  hasCart = true,
+}) => {
   const [openAuthModal, setOpenAuthModal] = React.useState(false);
   const searchParams = useSearchParams();
   const router = useRouter();
 
   React.useEffect(() => {
-    let toastMessage = '';
+    let toastMessage = "";
 
-    if (searchParams.has('verified')) {
-      toastMessage = 'Почта успешно подтверждена!';
+    if (searchParams.has("verified")) {
+      toastMessage = "Почта успешно подтверждена!";
     }
 
-    if (searchParams.has('paid')) {
-      toastMessage = 'Заказ успешно оплачен! Информация отправлена на почту.';
+    if (searchParams.has("paid")) {
+      toastMessage = "Заказ успешно оплачен! Информация отправлена на почту.";
     }
 
     if (toastMessage) {
       setTimeout(() => {
-        router.replace('/');
+        router.replace("/");
         toast.success(toastMessage, {
           duration: 3000,
         });
@@ -48,23 +52,42 @@ export const Header: React.FC<Props> = ({ className, hasSearch = true, hasCart =
   const onClickOpenAuthModal = () => setOpenAuthModal(true);
 
   return (
-    <header className={cn('border-b border-gray-100', className)}>
+    <header className={cn("border-b border-gray-100", className)}>
       <Container className="flex items-center justify-between py-8">
         <Link href="/">
           <div className="flex items-center gap-4">
             <Image src="/logo.png" width={35} height={35} alt="Logo" />
             <div>
-              <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-              <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+              {/* <h1 className="text-2xl uppercase font-black">Next Pizza</h1> */}
+              {/* <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p> */}
             </div>
           </div>
         </Link>
+        <nav>
+          <ul>
+            <li>
+              <a className="top1" href="/sherbet/aboutus">
+                О НАС
+              </a>
+            </li>
+            <li>
+              <a className="top2 submenu-link" href="/sherbet/store">
+                МАГАЗИН
+              </a>
+            </li>
+            <li>
+              <a className="top3 submenu-link" href="/sherbet/contacts">
+                КОНТАКТЫ
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-        {hasSearch && (
+        {/* {hasSearch && (
           <div className="mx-10 flex-1">
             <SearchInput />
           </div>
-        )}
+        )} */}
 
         <div className="flex items-center gap-3">
           <AuthModal open={openAuthModal} onClose={onClickOpenAuthModal} />
