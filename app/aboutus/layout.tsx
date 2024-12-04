a79529c7c015c9c9aea6f48938c67ca3159fd843
Seyran Japarov <japarov.s.i20@gmail.com>
2024-12-04 03:56:01 +0300

    определение layout

--- app/aboutus/layout.tsx
+++ app/aboutus/layout.tsx
@@ -1,23 +1,17 @@
-import type { Metadata } from "next";
-import { Header } from "@/components/shared/header";
+import React, { ReactNode } from 'react';
 
-export const metadata: Metadata = {
-  title: "Sherbet | Сладости востока",
-  description: "Магазин восточных сладостей",
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <header>Header Content</header>
+      <main>{children}</main>
+      <footer>Footer Content</footer>
+    </div>
+  );
 };
 
-export default function RootLayout({
-  children,
-  modal,
-}: Readonly<{
-  children: React.ReactNode;
-  modal: React.ReactNode;
-}>) {
-  return (
-    <main className="min-h-screen bg-[#404040]">
-      <Header />
-      {children}
-      {modal}
-    </main>
-  );
-}
+export default Layout; // Правильный экспорт
